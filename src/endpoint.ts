9a50b9fb881976f2a4a9512ff40453c187750f78
iGalaxyYT <contact@igalaxy.dev>
2022-03-07 20:49:06 -0800

    feat: endpoint

--- src/endpoint.ts
+++ src/endpoint.ts
@@ -1,1 +1,17 @@
-export function endpoint(auth: string, endpoint: string) {}
+import fetch from 'node-fetch';
+
+import { getPlaytime } from './index';
+
+export function endpoint(auth: string, endpoint: string) {
+	const playtime = getPlaytime();
+
+	fetch(endpoint, {
+		method: 'POST',
+		headers: {
+			Authorization: auth,
+		},
+		body: `${playtime}`,
+	}).then(async res => {
+		console.log(await res.text());
+	});
+}
